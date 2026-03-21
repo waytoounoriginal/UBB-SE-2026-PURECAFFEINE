969d4a3c7966ad3e1cdb3b526bb2d292aeebf430
waytoounoriginal <mihai.tira@yahoo.ro>
2026-03-21 16:19:24 +0200

    Add messges to the library

--- ServerCommunication/Interfaces/IMessage.cs
+++ ServerCommunication/Interfaces/IMessage.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ServerCommunication.Interfaces
 {
-    internal class IMessage
+    public interface IMessage
     {
+        string MessageType { get; }
+
+        /// <summary>
+        /// Provides the MessageWrapper for a given Message
+        /// </summary>
+        /// <returns>A MessageWrapper instance containing the payload of the object</returns>
+        MessageWrraper ToMessage()
+        {
+            return new MessageWrraper
+            {
+                Type = MessageType,
+                Payload = JsonSerializer.SerializeToUtf8Bytes(this)
+            };
+        }
     }
 }
--- ServerCommunication/SendNotificationMessage.cs
+++ ServerCommunication/SendNotificationMessage.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServerCommunication.Interfaces;
 
 namespace ServerCommunication
 {
-    internal class SendNotificationMessage
+    public class SendNotificationMessage : IMessage
     {
+        public string MessageType => nameof(SendNotificationMessage);
+
+        public int UserId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
     }
 }
